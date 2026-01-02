e08403d0f758cb3c206b13e98727574a522471e2
marshall <1332775752@qq.com>
2026-01-02 23:06:04 +0800

    [P-1] fix bug that BaseLazyColumnItem click background did not fill full line

--- app/src/main/java/com/marshall/pyerite/ui/golbalComponents/BaseContainer.kt
+++ app/src/main/java/com/marshall/pyerite/ui/golbalComponents/BaseContainer.kt
@@ -1,7 +1,7 @@
 package com.marshall.pyerite.ui.golbalComponents
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +10,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,7 +21,7 @@ import com.marshall.pyerite.R
 @Composable
 fun BaseContainer(
     title: String? = null,
-    modifier: Modifier = Modifier,
+    @SuppressLint("ModifierParameter") modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
     Column(modifier = modifier.systemBarsPadding()) {
@@ -30,26 +31,21 @@ fun BaseContainer(
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Black,
                 color = colorResource(R.color.text_primary),
-                modifier = Modifier.padding(bottom = 4.dp).padding(start = 24.dp)
+                modifier = Modifier.padding(start = 24.dp, bottom = 4.dp)
             )
         }
 
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .then(
-                    Modifier.padding(horizontal = 16.dp)
-                )
+                .padding(horizontal = 16.dp)
                 .background(
-                    color = colorResource(R.color.second_background), // second_background
+                    color = colorResource(R.color.second_background),
                     shape = RoundedCornerShape(16.dp)
                 )
+                .clip(RoundedCornerShape(16.dp))
         ) {
-            Box(
-                modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
-            ) {
-                content()
-            }
+            content()
         }
     }
 }
--- app/src/main/java/com/marshall/pyerite/ui/golbalComponents/BaseLazyColumn.kt
+++ app/src/main/java/com/marshall/pyerite/ui/golbalComponents/BaseLazyColumn.kt
@@ -2,12 +2,9 @@ package com.marshall.pyerite.ui.golbalComponents
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -48,58 +45,50 @@ fun BaseLazyColumnItem(
     model: BaseLazyColumnItemModel,
     showDivider: Boolean,
 ) {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(IntrinsicSize.Min)
             .clickable { model.onClick() }
     ) {
 
-        Icon(
+        Row(
             modifier = Modifier
-                .padding(vertical = 8.dp)
-                .size(24.dp),
-            painter = painterResource(model.iconRes),
-            contentDescription = null,
-        )
-
-        Spacer(modifier = Modifier.width(4.dp))
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = model.itemName,
-                    color = colorResource(R.color.text_primary),
-                )
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-                    contentDescription = null,
-                    tint = colorResource(R.color.hint_text),
-                )
-            }
+            Icon(
+                modifier = Modifier.size(24.dp),
+                painter = painterResource(model.iconRes),
+                contentDescription = null,
+            )
 
-            if (showDivider) {
-                ItemDivider()
-            }
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                modifier = Modifier.weight(1f),
+                text = model.itemName,
+                color = colorResource(R.color.text_primary),
+            )
+
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = null,
+                tint = colorResource(R.color.hint_text),
+            )
         }
+
+        if (showDivider) ItemDivider()
     }
 }
 
 @Composable
 private fun ItemDivider() {
     HorizontalDivider(
-        modifier = Modifier.fillMaxWidth() ,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp),
         thickness = 0.5.dp,
         color = colorResource(R.color.border),
     )
